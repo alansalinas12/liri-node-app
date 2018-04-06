43e78814ddb5f878d981c2a68707effddab34613
Alan Salinas <alanrene19@gmail.com>
2018-04-06 15:50:17 -0500

    final

--- liri.js
+++ liri.js
@@ -35,6 +35,58 @@ switch (command) {
 }
 
 
+function searchSpotify() {
+
+    if (!trackName) {
+        trackName = "ace of base the sign";
+    }
+    spotify.search({ type: 'track', query: trackName, limit: 1 }).then(response => {
+
+            if (JSON.stringify(response.tracks.items[0])) {
+                let trackObj = response.tracks.items[0];
+                console.log("\nSpotify Song Information:\n" + "-".repeat(25) + "\n");
+                console.log("Artist(s):\n" + "-".repeat(10));
+                trackObj.artists.forEach(function (artist) {
+                console.log(artist.name);
+                });
+                console.log("\nSong Title:\n" + "-".repeat(11) + "\n" + trackObj.name);
+
+                if (trackObj.preview_url !== null) {
+                    console.log("\nPreview Link:\n" + "-".repeat(13) + "\n" + trackObj.preview_url);
+                }
+                console.log("\nAlbum Title:\n" + "-".repeat(12) + "\n" + trackObj.album.name);
+            }
+        })
+}
+
+function searchMovie() {
+    let Url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    if (!movieName) {
+        movieName = "mr nobody";
+    }
+
+rp(Url).then(response => {
+            let movieObj = JSON.parse(response);
+
+            if (movieObj.Error === undefined) {
+                console.log("\nHere's a summary of the movie you searched for:\n" + "-".repeat(47));
+                console.log("\nTitle:\n" + "-".repeat(6) + "\n" + movieObj.Title);
+                console.log("\nRelease Year:\n" + "-".repeat(13) + "\n" + movieObj.Year);
+
+                if (movieObj.Ratings[0]) {
+                    console.log("\n" + movieObj.Ratings[0].Source + " Rating:\n" + "-".repeat(31) + "\n" + movieObj.Ratings[0].Value);
+                }
+                if (movieObj.Ratings[1]) {
+                    console.log("\n" + movieObj.Ratings[1].Source + " Rating:\n" + "-".repeat(23) + "\n" + movieObj.Ratings[1].Value);
+                }
+                console.log("\nCountry of Origin:\n" + "-".repeat(18) + "\n" + movieObj.Country);
+                console.log("\nLanguage:\n" + "-".repeat(9) + "\n" + movieObj.Language);
+                console.log("\nPlot Summary:\n" + "-".repeat(13) + "\n" + movieObj.Plot);
+                console.log("\nStarring:\n" + "-".repeat(9) + "\n" + movieObj.Actors);
+                }
+            })
+        }
 
 function random() {
     fs.readFile('random.txt', 'utf8').then(data => {
@@ -56,4 +108,17 @@ function random() {
         })
 }
 
+function getTweets() {
+    let params = { screen_name: 'BigAdain', count: 20, tweet_mode: 'extended' };
 
+    client.get('statuses/user_timeline/', params).then(tweets => {
+        let counter = tweets.length;
+        console.log("\nThe last " + counter + " tweets from " + tweets[0].user.name + " in ascending order:\n" + "-".repeat(60));
+
+        tweets.forEach(function (tweet) {
+            console.log("\nTweet #" + counter + "\n" + "-".repeat(10));
+            console.log(moment(tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').format('X') + "\n");
+            counter--;
+        });
+    })
+}
